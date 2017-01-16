43574897f8f3b602fa8821ac204030fe683c11a3
Leon <leon.karho.chung@gmail.com>
2017-01-16 14:58:27 -0500

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+# 1.How would you calculate a good tip for a $55 meal?
+puts 55 * 0.15
+
+
+# 2.Try adding a string and an integer with the + operator. Find a way to convert the integer into a string first and use puts to print the result.
+puts "i want to eat #{5} bagels"
+
+
+# Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+puts "#{45628 * 7839}"
