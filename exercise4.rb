f6acfe7449a1603a595727200635dbdaec01f776
Leon <leon.karho.chung@gmail.com>
2017-01-16 16:31:45 -0500

    exercise4.rb

---
+++ exercise4.rb
@@ -0,0 +1,15 @@
+number = 1
+
+while number <= 100
+
+  if (number % 3 == 0 && number % 5 == 0)
+    puts "BitMaker"
+  elsif number % 3 == 0
+    puts "Bit"
+  elsif number % 5 == 0
+    puts "Maker"
+  else
+    puts "#{number}"
+  end
+  number += 1
+end
